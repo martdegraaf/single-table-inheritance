049977225a77da559be10fb3306b35e6f6ed6c4b
Mart de Graaf <mart@martdegraaf.nl>
2015-12-22 00:39:25 +0100

    Fixed when the value is 0
    Example when using an int Enum for SingleTableInheritance.
    class SingleTableTypes {
    const VARIABLE1 = 0;
    const VARIABLE2 = 1;
    const VARIABLE3 = 2;
    const VARIABLE4= 3;
    }
    
    When the value was zero it would go trough line 175 on "if($classType)"
    when $classType was 0. But was actually valid.

--- src/SingleTableInheritanceTrait.php
+++ src/SingleTableInheritanceTrait.php
@@ -174,7 +174,7 @@ trait SingleTableInheritanceTrait {
 
     $classType = isset($attributes->$typeField) ? $attributes->$typeField : null;
 
-    if ($classType) {
+    if ($classType !=== null) {
       $childTypes = static::getSingleTableTypeMap();
       if (array_key_exists($classType, $childTypes)) {
         $class = $childTypes[$classType];
